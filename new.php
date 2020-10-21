dd4523506c133d308aab39b63d87f73b1154b9c8
Rakibul-Islam-GitHub <freelancing.everyday@gmail.com>
2020-10-22 03:01:38 +0600

    pass json obj from php

---
+++ new.php
@@ -0,0 +1,15 @@
+<?php
+$return_arr = array();
+//$name= $_POST['name'];
+
+    $id ="123";
+    $name = "Rakibul";
+    
+    //$email = $row['email'];
+
+    $assoarr = ["name"=>"Rakibul", "id"=>123];
+
+    echo json_encode($assoarr);
+
+
+?>
